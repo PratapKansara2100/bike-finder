096bceed76faf9ec73b21fe9f60264188b86c015
PratapK <pratakansara24@gmail.com>
2023-06-25 06:16:18 -0700

    removed unused imports and variables from map.tsx

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,16 +1,14 @@
-import { useState, useMemo, useCallback, useRef } from 'react';
+import { useMemo } from 'react';
 import {
     GoogleMap,
-    Marker,
-    DirectionsRenderer,
-    Circle,
-    MarkerClusterer,
+    // Marker,
+    // DirectionsRenderer,
+    // Circle,
+    // MarkerClusterer,
 } from '@react-google-maps/api';
 // import Places from './places';
 // import Distance from './distance';
 import './Map.css';
-import { debug } from 'console';
-
 type LatLngLiteral = google.maps.LatLngLiteral;
 // type DirectionsResult = google.maps.DirectionsResult;
 // type MapOptions = google.maps.MapOptions;
@@ -41,45 +39,45 @@ function Map() {
     );
 }
 
-const defaultOptions = {
-    strokeOpacity: 0.5,
-    strokeWeight: 2,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    visible: true,
-};
-const closeOptions = {
-    ...defaultOptions,
-    zIndex: 3,
-    fillOpacity: 0.05,
-    strokeColor: '#8BC34A',
-    fillColor: '#8BC34A',
-};
-const middleOptions = {
-    ...defaultOptions,
-    zIndex: 2,
-    fillOpacity: 0.05,
-    strokeColor: '#FBC02D',
-    fillColor: '#FBC02D',
-};
-const farOptions = {
-    ...defaultOptions,
-    zIndex: 1,
-    fillOpacity: 0.05,
-    strokeColor: '#FF5252',
-    fillColor: '#FF5252',
-};
+// const defaultOptions = {
+//     strokeOpacity: 0.5,
+//     strokeWeight: 2,
+//     clickable: false,
+//     draggable: false,
+//     editable: false,
+//     visible: true,
+// };
+// const closeOptions = {
+//     ...defaultOptions,
+//     zIndex: 3,
+//     fillOpacity: 0.05,
+//     strokeColor: '#8BC34A',
+//     fillColor: '#8BC34A',
+// };
+// const middleOptions = {
+//     ...defaultOptions,
+//     zIndex: 2,
+//     fillOpacity: 0.05,
+//     strokeColor: '#FBC02D',
+//     fillColor: '#FBC02D',
+// };
+// const farOptions = {
+//     ...defaultOptions,
+//     zIndex: 1,
+//     fillOpacity: 0.05,
+//     strokeColor: '#FF5252',
+//     fillColor: '#FF5252',
+// };
 
-const generateHouses = (position: LatLngLiteral) => {
-    const _houses: Array<LatLngLiteral> = [];
-    for (let i = 0; i < 100; i++) {
-        const direction = Math.random() < 0.5 ? -2 : 2;
-        _houses.push({
-            lat: position.lat + Math.random() / direction,
-            lng: position.lng + Math.random() / direction,
-        });
-    }
-    return _houses;
-};
+// const generateHouses = (position: LatLngLiteral) => {
+//     const _houses: Array<LatLngLiteral> = [];
+//     for (let i = 0; i < 100; i++) {
+//         const direction = Math.random() < 0.5 ? -2 : 2;
+//         _houses.push({
+//             lat: position.lat + Math.random() / direction,
+//             lng: position.lng + Math.random() / direction,
+//         });
+//     }
+//     return _houses;
+// };
 export default Map;
